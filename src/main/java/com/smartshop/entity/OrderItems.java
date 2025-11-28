d881fb1e958b949e2384b4d26458c9e9edf3843e
yassine <yassineaghla23@gmail.com>
2025-11-28 16:38:24 +0100

    supprimer orderitems et faire un push apres les commit dans local

--- src/main/java/com/smartshop/entity/OrderItems.java
+++
@@ -1,4 +0,0 @@
-package com.smartshop.entity;
-
-public class OrderItems {
-}
--- src/main/java/com/smartshop/services/OrderService.java
+++ src/main/java/com/smartshop/services/OrderService.java
@@ -13,7 +13,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDateTime;
